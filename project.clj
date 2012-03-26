c4728e765986b759428958fb8f852b12e4d8269d
Corbin Fox <corbin.fox@gmail.com>
2012-03-26 16:46:05 -0700

    Added lein run alias for extractor.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/data.json "0.1.2"]
                  [congomongo "0.1.8"]
-		 [ring/ring-jetty-adapter "1.0.1"]])
+		 [ring/ring-jetty-adapter "1.0.1"]]
+  :run-aliases {:extractor mc.extractor})
